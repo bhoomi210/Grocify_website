c4936f0dc095193f6d001f38956babf50cc8c7e8
Bhoomi Purohit <bhumirpurohit@gmail.com>
2026-05-09 13:57:28 +0530

    fixed import path

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Hero from './Hero'
 import Category from './Category'
-import Products from './products'
+import Products from './Products'
 import Discount from './Discount'
 import Process from './Process'
 import Testimonials from './Testimonials'
-import Values from './values'
+import Values from './Values'
 
 const Home = () => {
   return (
